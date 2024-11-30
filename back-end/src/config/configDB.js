0439e858b5f0f973fc1ba5745ba73407bfc1cbd8
LFGuzman2105 <luis.guzmanorozco@galileo.edu>
2024-11-29 19:11:59 -0600

    dockerfile backend

--- back-end/src/config/configDB.js
+++ back-end/src/config/configDB.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({ 
-    host: 'localhost',
+    host: '34.70.107.153',
     user: 'root',
-    password: '',
+    password: 'pokemon12',
     database: 'bancocc6'
 });
 
